dd5028e15f88eb5cb4e7a62b0da541ef24298e4f
haikali3 <manfdvcL9@gmail.com>
2024-01-14 18:56:05 +0800

    feat:Refactor Card and CardContent components, fetch worldwide data in WorldwideData component

---
+++ src/components/MainCard.jsx
@@ -0,0 +1,11 @@
+const MainCard = ({ children, addClass }) => {
+  return (
+    <div
+      className={`mx-2 h-fit w-full space-y-2 rounded-md border border-gray-300 p-6 shadow lg:w-auto ${addClass}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MainCard;
